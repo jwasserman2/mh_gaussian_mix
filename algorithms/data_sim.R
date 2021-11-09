86edcd82996181620882dcc1e9804df0f89c6882
jwasserman2 <joshua.wasserman777@gmail.com>
2021-11-08 22:18:52 -0500

    BUG remove heavytail option for rw_metropolis

--- algorithms/data_sim.R
+++ algorithms/data_sim.R
@@ -1,9 +1,11 @@
 #' Calculate the true density for a mixture distribution
-calculate_true_density <- function(x, gaussian_mixture, mu, sigma, mixture_probs) {
-  if (gaussian_mixture) {
-    return(calculate_gaussian_mixture_prob(x, mu, sigma, mixture_probs))
+calculate_true_density <- function(x, true_density_kwargs) {
+  run_kwargs <- true_density_kwargs[names(true_density_kwargs) != "gaussian_mixture"]
+  run_kwargs["x"] <- x
+  if (true_density_kwargs[["gaussian_mixture"]]) {
+    return(do.call(calculate_gaussian_mixture_prob, run_kwargs))
   } else {
-    return(calulate_heavytail_mixture_prob(x, mu, sigma, mixture_probs))
+    return(do.call(calulate_heavytail_mixture_prob, run_kwargs))
   }
 }
 
@@ -14,4 +16,15 @@ calculate_gaussian_mixture_prob <- function(x, mu, sigma, mixture_probs) {
     rlang::abort("Mu, sigma, and mixture probabilities must have corresponding shapes")
   }
   return((dnorm(x, mu, sigma) %*% mixture_probs)[1, 1])
+}
+
+#' Calculate T mixture density
+calculate_heavytail_mixture_prob <- function(x, df, noncentral_param, mixture_probs) {
+  vec_lengths <- purrr::map_dbl(list(df, noncentral_param, mixture_probs), length)
+  if (any(vec_lengths != vec_lengths[1])) {
+    msg <- paste0("Degrees of freedom, noncentral parameters, and mixture probabilities ",
+                  "must have corresponding shapes")
+    rlang::abort(msg)
+  }
+  return((dt(x, df = df, ncp = noncentral_param) %*% mixture_probs)[1, 1])
 }
--- algorithms/rw_metropolis.R
+++ algorithms/rw_metropolis.R
@@ -4,17 +4,22 @@ source("data_sim.R")
 #' Proposal dist q(x, y) for x, y in R_{d} at time t: x_{t-1} + epsilon
 #' Epsilon ~ spherically symmetric with variance = sigma^2
 #' @param n_dim int
-#' @param true_density_args list, must include "gaussian_mixture", "mu", "sigma", and "mixture_probs"
-rw_metropolis <- function(n_dim, gaussian_mix, mu, sigma, mixture_probs, n_iters) {
+#' @param mu numeric vector, component means for mixture distribution
+#' @param sigma numeric vector, component variances for mixture distribution
+#' @param mixture_probs numeric vector, mixture probabilities for mixture distribution
+#' @param n_iters int, number of steps in the chain
+#' 
+#' @return matrix of steps in the chain
+rw_metropolis <- function(n_dim, mu, sigma, mixture_probs, n_iters) {
   chain <- runif(n_dim)
   message(paste0("Running Random Walk Metropolis chain with ", n_iters, " steps"))
   for (i in 1:n_iters) {
     if (i %% 1000 == 0) {
       message(paste0("Proposing step ", i))
     }
-    proposal <- propose(chain[i])
-    acceptance_prob <- min(1, calculate_true_density(proposal, gaussian_mix, mu, sigma, mixture_probs) /
-                             calculate_true_density(chain[i], gaussian_mix, mu, sigma, mixture_probs))
+    proposal <- chain[i] + rnorm(n_dim)
+    acceptance_prob <- min(1, calculate_gaussian_mixture_prob(proposal, mu, sigma, mixture_probs) /
+                             calculate_gaussian_mixture_prob(chain[i], mu, sigma, mixture_probs))
     accept <- as.logical(rbinom(1, 1, acceptance_prob))
     if (accept) {
       chain <- rbind(chain, proposal, deparse.level = 0)
@@ -25,7 +30,9 @@ rw_metropolis <- function(n_dim, gaussian_mix, mu, sigma, mixture_probs, n_iters
   return(chain)
 }
 
-#' Make a spherically symmetric proposal (using multivariate standard normal)
+#' Make a spherically symmetric proposal
+#' Propose a step in Random Walk Metropolis chain using multivariate normal with uncorrelated
+#' standard normal components
 #' @param pos_vec numeric vector
 #' 
 #' @return numeric vector of pos_vec + proposed steps
@@ -46,10 +53,13 @@ propose <- function(pos_vec) {
   return(pos_vec + steps)
 }
 
-mu_vec <- c(-5, 0, 5)
-sigma_vec <- rep(2, 3)
-mixture_probs <- c(0.45, 0.1, 0.45)
-chain <- rw_metropolis(1, TRUE, mu_vec, sigma_vec, mixture_probs, 5000)
+true_gaussian_kwargs <- list(
+  "gaussian_mixture" = TRUE,
+  "mu" = c(-5, 0, 5),
+  "sigma" = rep(2, 3),
+  "mixture_probs" = c(0.45, 0.1, 0.45)
+)
+chain <- rw_metropolis(1, c(-5, 0, 5), rep(2, 3), c(0.45, 0.1, 0.45), 5000)
 ggplot2::ggplot(data.frame("chain" = chain)) +
   ggplot2::geom_histogram(ggplot2::aes(x = chain), bins = 30) +
   ggplot2::geom_line(
@@ -57,13 +67,29 @@ ggplot2::ggplot(data.frame("chain" = chain)) +
                       "true_density" = purrr::map_dbl(
                         seq(min(mu_vec) - 4 * max(sigma_vec), max(mu_vec) + 4 * max(sigma_vec), 0.1),
                         calculate_gaussian_mixture_prob,
-                        mu_vec,
-                        sigma_vec,
-                        mixture_probs
+                        c(-5, 0, 5),
+                        rep(2, 3),
+                        c(0.45, 0.1, 0.45)
                       )),
-    ggplot2::aes(x = x, y = true_density * 10000)) +
+    ggplot2::aes(x = x, y = true_density * 5000)) +
   ggplot2::scale_x_continuous(
     breaks = round(seq(min(mu_vec) - 4 * max(sigma_vec), max(mu_vec) + 4 * max(sigma_vec), 1)))
+
+ggplot2::ggplot(data.frame("chain" = chain)) +
+  ggplot2::geom_histogram(ggplot2::aes(x = chain), bins = 30) +
+  ggplot2::geom_line(
+    data = data.frame("x" = seq(min(mu_vec) - 4 * max(sigma_vec), max(mu_vec) + 4 * max(sigma_vec), 0.1),
+                      "true_density" = purrr::map_dbl(
+                        seq(min(mu_vec) - 4 * max(sigma_vec), max(mu_vec) + 4 * max(sigma_vec), 0.1),
+                        calculate_heavytail_mixture_prob,
+                        rep(10, 3),
+                        c(-5, 0, 5), 
+                        c(.45, .1, .45)
+                      )),
+    ggplot2::aes(x = x, y = true_density * 5000)) +
+  ggplot2::scale_x_continuous(
+    breaks = round(seq(-4, 4, 0.1)))
+
 
 theta_2_chain <- rw_metropolis(2, TRUE, mu_vec, sigma_vec, mixture_probs, 10000)
 ggplot2::ggplot(data.frame("chain" = theta_2_chain[,2])) +
