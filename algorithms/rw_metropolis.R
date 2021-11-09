e3c50f1130b6379a2e8b15f3e10d04057171a16a
jwasserman2 <joshua.wasserman777@gmail.com>
2021-11-09 00:02:00 -0500

    ADD seed to rw_metropolis

--- algorithms/rw_metropolis.R
+++ algorithms/rw_metropolis.R
@@ -2,22 +2,28 @@ source("constants.R")
 source("data_sim.R")
 #' Run Random Walk Metropolis Metropolis-Hastings Algorithm
 #' Proposal dist q(x, y) for x, y in R_{d} at time t: x_{t-1} + epsilon
-#' Epsilon ~ spherically symmetric with variance = sigma^2
+#' Epsilon ~ N(0, proposal_sigma)
 #' @param n_dim int
 #' @param mu numeric vector, component means for mixture distribution
 #' @param sigma numeric vector, component variances for mixture distribution
 #' @param mixture_probs numeric vector, mixture probabilities for mixture distribution
+#' @param proposal_sigma float, variance for multivariate normal proposal distribution
 #' @param n_iters int, number of steps in the chain
+#' @param seed int
 #' 
 #' @return matrix of steps in the chain
-rw_metropolis <- function(n_dim, mu, sigma, mixture_probs, n_iters) {
+rw_metropolis <- function(n_dim, mu, sigma, mixture_probs, proposal_sigma, n_iters, seed = NA) {
+  if (is.na(seed)) {
+    seed <- round(runif(1) * 1e7)
+  }
+  set.seed(seed)
   chain <- runif(n_dim)
-  message(paste0("Running Random Walk Metropolis chain with ", n_iters, " steps"))
+  message(paste0("Running Random Walk Metropolis chain with ", n_iters, " steps and seed: ", seed))
   for (i in 1:n_iters) {
     if (i %% 1000 == 0) {
       message(paste0("Proposing step ", i))
     }
-    proposal <- chain[i] + rnorm(n_dim)
+    proposal <- chain[i] + rnorm(n_dim, sd = sqrt(proposal_sigma))
     acceptance_prob <- min(1, calculate_gaussian_mixture_prob(proposal, mu, sigma, mixture_probs) /
                              calculate_gaussian_mixture_prob(chain[i], mu, sigma, mixture_probs))
     accept <- as.logical(rbinom(1, 1, acceptance_prob))
@@ -52,57 +58,3 @@ propose <- function(pos_vec) {
   # return(pos_vec + steps[iterate_order])
   return(pos_vec + steps)
 }
-
-true_gaussian_kwargs <- list(
-  "gaussian_mixture" = TRUE,
-  "mu" = c(-5, 0, 5),
-  "sigma" = rep(2, 3),
-  "mixture_probs" = c(0.45, 0.1, 0.45)
-)
-chain <- rw_metropolis(1, c(-5, 0, 5), rep(2, 3), c(0.45, 0.1, 0.45), 5000)
-ggplot2::ggplot(data.frame("chain" = chain)) +
-  ggplot2::geom_histogram(ggplot2::aes(x = chain), bins = 30) +
-  ggplot2::geom_line(
-    data = data.frame("x" = seq(min(mu_vec) - 4 * max(sigma_vec), max(mu_vec) + 4 * max(sigma_vec), 0.1),
-                      "true_density" = purrr::map_dbl(
-                        seq(min(mu_vec) - 4 * max(sigma_vec), max(mu_vec) + 4 * max(sigma_vec), 0.1),
-                        calculate_gaussian_mixture_prob,
-                        c(-5, 0, 5),
-                        rep(2, 3),
-                        c(0.45, 0.1, 0.45)
-                      )),
-    ggplot2::aes(x = x, y = true_density * 5000)) +
-  ggplot2::scale_x_continuous(
-    breaks = round(seq(min(mu_vec) - 4 * max(sigma_vec), max(mu_vec) + 4 * max(sigma_vec), 1)))
-
-ggplot2::ggplot(data.frame("chain" = chain)) +
-  ggplot2::geom_histogram(ggplot2::aes(x = chain), bins = 30) +
-  ggplot2::geom_line(
-    data = data.frame("x" = seq(min(mu_vec) - 4 * max(sigma_vec), max(mu_vec) + 4 * max(sigma_vec), 0.1),
-                      "true_density" = purrr::map_dbl(
-                        seq(min(mu_vec) - 4 * max(sigma_vec), max(mu_vec) + 4 * max(sigma_vec), 0.1),
-                        calculate_heavytail_mixture_prob,
-                        rep(10, 3),
-                        c(-5, 0, 5), 
-                        c(.45, .1, .45)
-                      )),
-    ggplot2::aes(x = x, y = true_density * 5000)) +
-  ggplot2::scale_x_continuous(
-    breaks = round(seq(-4, 4, 0.1)))
-
-
-theta_2_chain <- rw_metropolis(2, TRUE, mu_vec, sigma_vec, mixture_probs, 10000)
-ggplot2::ggplot(data.frame("chain" = theta_2_chain[,2])) +
-  ggplot2::geom_histogram(ggplot2::aes(x = chain), bins = 30) +
-  ggplot2::geom_line(
-    data = data.frame("x" = seq(min(mu_vec) - 4 * max(sigma_vec), max(mu_vec) + 4 * max(sigma_vec), 0.1),
-                      "true_density" = purrr::map_dbl(
-                        seq(min(mu_vec) - 4 * max(sigma_vec), max(mu_vec) + 4 * max(sigma_vec), 0.1),
-                        calculate_gaussian_mixture_prob,
-                        mu_vec,
-                        sigma_vec,
-                        mixture_probs
-                      )),
-    ggplot2::aes(x = x, y = true_density * 10000)) +
-  ggplot2::scale_x_continuous(
-    breaks = round(seq(min(mu_vec) - 4 * max(sigma_vec), max(mu_vec) + 4 * max(sigma_vec), 1)))
