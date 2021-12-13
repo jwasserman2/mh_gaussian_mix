e7d378fffaf4c0485ce9738e22751478054af3db
jwasserman2 <joshua.wasserman777@gmail.com>
2021-12-13 10:32:09 -0500

    submitted project, everything in analysis script

--- algorithms/data_sim.R
+++ algorithms/data_sim.R
@@ -55,6 +55,33 @@ generate_normal_mixture_data <- function(n, mu, sigma, mixture_probs, random = T
   return(data)
 }
 
+#' Generate gaussian mix in multiple dimensions
+generate_normal_d_mixture_data <- function(n, mu, sigma_list, mixture_probs, random = TRUE, seed = NA) {
+  if (is.na(seed) & random) {
+    seed <- round(runif(1), 7) * 10e7
+    set.seed(seed)
+    message(paste0("Seed set to ", seed))
+  }
+  
+  if (random) {
+    set.seed(seed)
+    dist_idx <- sample(1:length(mixture_probs), n, replace = TRUE, prob = mixture_probs)
+    counts <- data.frame(table(dist_idx))
+    print(counts$Freq[counts$dist_idx == 1])
+    data <- MASS::mvrnorm(counts$Freq[counts$dist_idx == 1], mu[1,], sigma_list[[1]])
+    for (i in 2:length(mixture_probs)) {
+      data <- rbind(data, MASS::mvrnorm(counts$Freq[counts$dist_idx == i], mu[i,], sigma_list[[i]]))
+    }
+  } else {
+    set.seed(2045)
+    dist_idx <- sample(1:length(mixture_probs), N_FOR_CDF, replace = TRUE, prob = mixture_probs)
+    cdf <- purrr::map2_dbl(mu[dist_idx], sigma[dist_idx], rnorm, n = 1)
+    data <- sample(cdf, n)
+  }
+  
+  return(data)
+}
+
 #' Generate a random sample or empirical cdf from a T mixture 
 generate_t_mixture_data <- function(n, ncp, df, mixture_probs, random = TRUE, seed = NA) {
   if (is.na(seed) & random) {
@@ -71,6 +98,33 @@ generate_t_mixture_data <- function(n, ncp, df, mixture_probs, random = TRUE, se
   if (random) {
     dist_idx <- sample(1:length(mixture_probs), n, replace = TRUE, prob = mixture_probs)
     data <- purrr::map2_dbl(df[dist_idx], ncp[dist_idx], rt, n = 1)
+  } else {
+    set.seed(2045)
+    dist_idx <- sample(1:length(mixture_probs), N_FOR_CDF, replace = TRUE, prob = mixture_probs)
+    cdf <- purrr::map2_dbl(df[dist_idx], ncp[dist_idx], rt, n = 1)
+    data <- sample(cdf, n)
+  }
+  
+  return(data)
+}
+
+#' Generate a random sample or empirical cdf from a T mixture 
+generate_t_d_mixture_data <- function(n, ncp, df, mixture_probs, random = TRUE, seed = NA) {
+  if (is.na(seed) & random) {
+    seed <- round(runif(1), 7) * 10e7
+    set.seed(seed)
+    message(paste0("Seed set to ", seed))
+  }
+
+  if (random) {
+    set.seed(seed)
+    dist_idx <- sample(1:length(mixture_probs), n, replace = TRUE, prob = mixture_probs)
+    counts <- data.frame(table(dist_idx))
+    print(counts$Freq[counts$dist_idx == 1])
+    data <- mvtnorm::rmvt(counts$Freq[counts$dist_idx == 1], delta = ncp[1,], df = df[1])
+    for (i in 2:length(mixture_probs)) {
+      data <- rbind(data, mvtnorm::rmvt(counts$Freq[counts$dist_idx == i], delta = ncp[i,], df = df[i]))
+    }
   } else {
     set.seed(2045)
     dist_idx <- sample(1:length(mixture_probs), N_FOR_CDF, replace = TRUE, prob = mixture_probs)
--- algorithms/plots.R
+++ algorithms/plots.R
@@ -1,4 +1,5 @@
 source("./algorithms/data_sim.R")
+source("./algorithms/constants.R")
 #' Plot an MCMC chain against a Gaussian mixture distribution (currently only for one parameter of interest)
 plot_mcmc_vs_true_density <- function(chain, true_density_kwargs) {
   if (any(names(true_density_kwargs) == "df")) {
@@ -7,7 +8,7 @@ plot_mcmc_vs_true_density <- function(chain, true_density_kwargs) {
     true_density_func <- calculate_gaussian_mixture_prob
   }
   
-  n_iters <- dim(chain)[1]
+  n_iters <- length(chain)
   chain_df <- data.frame("chain" = chain)
   mu <- rlang::`%||%`(true_density_kwargs[["noncentral_param"]], true_density_kwargs[["mu"]])
   sigma <- rlang::`%||%`(true_density_kwargs[["df"]], true_density_kwargs[["sigma"]])
@@ -27,7 +28,7 @@ plot_mcmc_vs_true_density <- function(chain, true_density_kwargs) {
                            max(mu) + N_SDS_TO_PLOT * round(max(sqrt(sigma)), 1)))
 
   ggplot2::ggplot(chain_df) +
-    ggplot2::geom_histogram(ggplot2::aes(x = chain), bins = 30) +
+    ggplot2::geom_density(ggplot2::aes(x = chain)) +
     ggplot2::geom_line(data = true_df, ggplot2::aes(x = x, y = true_density * n_iters)) +
     ggplot2::scale_x_continuous(breaks = x_plot_breaks) +
     ggplot2::scale_y_continuous(labels = ~ .x / n_iters) +
